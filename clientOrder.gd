924d41d7abbdd46b970b59dfbf46d4bbdcff9f3c
Ben Geisler <bgeisler@gmail.com>
2026-03-08 15:31:41 -0700

    client orders

---
+++ clientOrder.gd
@@ -0,0 +1,7 @@
+class_name CharacterData
+extends Resource
+
+@export var character_name: String = ""
+@export var intro_line: String = ""
+@export var portrait: Texture2D = null
+@export var flowers: Dictionary[String, int] = {}

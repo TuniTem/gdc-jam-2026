1431e298f5ecb8b2e237d2e42b3a7cd8d9f4b2c4
Joseph Miller <josephclaymiller@users.noreply.github.com>
2026-03-08 20:35:47 -0700

    flower worth

--- Scenes/flower_button.gd
+++ Scenes/flower_button.gd
@@ -1,11 +1,14 @@
 extends TextureButton
 
 @export var flower_res : Resource = null
+@export var flower_cost: int = 0
 
 func _ready():
 	texture_normal = flower_res.side_texture
+	
 
 func _on_pressed():
 	Globals.selected_flower = texture_normal
 	Globals.selected_flower_res = flower_res
+
 	get_tree().call_group("flower_interface", "reset_flower_rotation")
